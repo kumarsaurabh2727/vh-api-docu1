e7ddd38926609e73974dc492763fb74dec2052da
kumarsaurabh2727 <kumarsaurabh2727@gmail.com>
2026-08-08 18:30:08 +0530

    Configure GITHub page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://kumarsaurabh2727.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/vh-api-docu1/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'kumarsaurabh2727', // Usually your GitHub org/user name.
+  projectName: 'vh-api-docu1', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
